b2ebce829849282f0a27df2efbab9090bfe3e94e
Augustus Otu <Augani7@gmail.com>
2020-05-13 12:19:16 +0000

    changed look

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 function Profile() {
     return (
         <div className="pMain">
-        profile
+        <div className="card">
+        <div className="heading">
+            
+        </div>
+        <div className="mid">
+
+        </div>
+        <div className="beneath">
+
+        </div>
+        </div>
         </div>
     )
 }
